8bc06adbc157ce6c7d1620813280d47a4a64927b
Ignacio Rivas <ignacio.rivas.u@gmail.com>
2025-04-08 02:53:40 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -99,6 +99,25 @@ resource "aws_db_subnet_group" "default" {
   subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_a" {
+  subnet_id      = aws_subnet.subnet_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_subnet_b" {
+  subnet_id      = aws_subnet.subnet_b.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_db_instance" "postgres" {
   identifier             = "flask-db"
   engine                 = "postgres"
